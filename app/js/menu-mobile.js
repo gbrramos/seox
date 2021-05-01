e33fe04aed37b459b7809026d8f4b98f2fcb7b8d
Gabriel Oliveira <gabriel.olv2002@gmail.com>
2021-05-01 16:44:44 -0300

    finish mobile

--- app/js/menu-mobile.js
+++ app/js/menu-mobile.js
@@ -9,22 +9,16 @@ function menu() {
 	width = screen.width;
 	console.log(width);
 	target = document.getElementById('menu');
-	if (width > 769) {
+	
 		testClassTarget = target.matches('.open');
 		if (testClassTarget == true) {
 			target.classList.remove("open");
 			target.style.left = '0px';
+
 		} else {
 			target.classList.add("open");
 			target.style.left = '-600px';
-		}
-	}else{
-		target.style.position = 'relative';
-		target.style.left = 'inherit';
-	}
-}
 
-function arrow(){
-	t = document.getElementsByClassName('link_gray');
-	console.log(t);
+		}
+	
 }
